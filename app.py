00d693af3d10caa3d2de1708726091be4306fec0
terry-ian <39936938+terry-ian@users.noreply.github.com>
2019-02-21 16:56:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -385,9 +385,9 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=content))
         return 0
-    if event.message.text == "開始玩":
+    if event.message.text == "來用我的功能":
         buttons_template = TemplateSendMessage(
-            alt_text='開始玩 template',
+            alt_text='來用我的功能 template',
             template=ButtonsTemplate(
                 title='選擇服務',
                 text='請選擇',
